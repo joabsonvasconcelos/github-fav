78f3800b177a4835a9ad8ac02d01c7ef000235d2
joabson <joabson.vasconcelos@icloud.com>
2024-07-11 09:00:34 -0300

    update

--- js/GithubUser.js
+++ js/GithubUser.js
@@ -2,6 +2,8 @@ export class GithubUser {
   static search(username) {
     const endpoint = `https://api.github.com/users/${username}`
 
+    console.log(endpoint)
+
     return fetch(endpoint)
     .then(data => data.json())
     .then(({ login, name, public_repos, followers }) => ({
